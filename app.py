292e528e1c7eef25ef35c4207f26cfd9d0267824
Sanket Borade <102898604+sanketborade@users.noreply.github.com>
2024-05-27 14:37:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -95,7 +95,7 @@ if uploaded_file is not None:
         st.write("Accuracy for KMeans:", accuracy_kmeans)
         st.write("Accuracy for Local Outlier Factor:", accuracy_lof)
         st.write("Accuracy for One-Class SVM:", accuracy_svm)
-        st.write("Accuracy for Isolation Forest (perturbed):", accuracy_iforest)
+        st.write("Accuracy for Isolation Forest :", accuracy_iforest)
 
     with tab2:
         st.header("Exploratory Data Analysis")
